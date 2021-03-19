098dcc0401885bd274c892bc5a5412c96ba88798
pikachupichu25 <80876690+pikachupichu25@users.noreply.github.com>
2021-03-19 18:04:39 +0700

    Add files via upload

---
+++ side-pane.js
@@ -0,0 +1,65 @@
+// var elems = {
+// 		nodes: [
+// 			{ data: { 
+// 				id: 'a', 
+// 				summary: 'This is Node A summary.',
+// 				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
+// 			} },
+// 			{ data: { 
+// 				id: 'b', 
+// 				summary: 'This is Node B summary.',
+// 				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
+// 			} }
+// 		],
+// 		edges: [
+// 			{ data: { id: 'ab', source: 'a', target: 'b' } }
+// 		]
+// 	}
+
+var toJson = function(obj){ return obj.json(); };
+var graphP = fetch('tippy-test-graph.json').then(toJson);
+Promise.all([graphP]).then(initCy);
+
+function initCy(then){
+	var elems = then[0]
+	var cy = window.cy = cytoscape({
+	container: document.getElementById('cy'),
+	style: [
+		{
+			selector: 'node',
+			style: {
+				'content': 'data(id)'
+			}
+		},
+
+		{
+			selector: 'edge',
+			style: {
+				'curve-style': 'bezier',
+				'target-arrow-shape': 'triangle',
+				'content': 'data(title)'
+			}
+		}
+	],
+
+	elements: elems,
+	layout: {
+		name: 'grid'
+	}
+	});
+
+	cy.on('tap', 'node', function(evt){
+		var node = this;
+		var output_text = "<h2>"+node.data("title")+"</h2>"+ node.data("summary")
+		document.getElementById("summary").innerHTML = output_text;
+		
+		// add condition: center if the node is at the margin
+		// cy.center(node)
+	});
+
+
+}
+
+
+
+
--- tippy-test.js
+++ tippy-test.js
@@ -1,56 +1,26 @@
-// var toJson = function(obj){ return obj.json(); };
-// var elems = fetch("./simple_graph.json").then(toJson);
+// var elems = {
+// 		nodes: [
+// 			{ data: { 
+// 				id: 'a', 
+// 				summary: 'This is Node A summary.',
+// 				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
+// 			} },
+// 			{ data: { 
+// 				id: 'b', 
+// 				summary: 'This is Node B summary.',
+// 				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
+// 			} }
+// 		],
+// 		edges: [
+// 			{ data: { id: 'ab', source: 'a', target: 'b' } }
+// 		]
+// 	}
 
-var cy = window.cy = cytoscape({
-	container: document.getElementById('cy'),
-
-	style: [
-		{
-			selector: 'node',
-			style: {
-				'content': 'data(id)'
-			}
-		},
-
-		{
-			selector: 'edge',
-			style: {
-				'curve-style': 'bezier',
-				'target-arrow-shape': 'triangle'
-			}
-		}
-	],
-
-	elements: //elems,
-	{
-		nodes: [
-			{ data: { 
-				id: 'a', 
-				summary: 'This is Node A summary.',
-				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
-			} },
-			{ data: { 
-				id: 'b', 
-				summary: 'This is Node B summary.',
-				full_text: 'This is a long text. This is a long text. This is a long text. This is a long text. This is a long text.'
-			} }
-		],
-		edges: [
-			{ data: { id: 'ab', source: 'a', target: 'b' } }
-		]
-	},
-
-	layout: {
-		name: 'grid'
-	}
-});
-
-// var a = cy.getElementById('a');
-// var b = cy.getElementById('b');
-// var ab = cy.getElementById('ab');
-
-// make the tippy from graph ele
+var tippy_obj = [] 
+var nested_tippy_obj = []
 var shortText
+var cy
+// function make the tippy from graph ele
 var makeTippy = function(ele, text){
 	var ref = ele.popperRef();
 
@@ -80,7 +50,7 @@ var makeTippy = function(ele, text){
 	return tip;
 };
 
-// make tippy from any target
+// function make tippy from any target
 var makeNestedTippy = function(target, text){
 	var nestedtip = tippy(target, {
 		content: function(){
@@ -93,30 +63,69 @@ var makeNestedTippy = function(target, text){
 	});
 	return nestedtip;
 }
+/////
 
- 
-// create tippy for every node
-var tippy_obj = [] 
-var nested_tippy_obj = []
-for (var i = 0; i < cy.nodes().size(); i++){
-	var node = cy.nodes()[i];
-	var tip = makeTippy(node, node.data('summary'));
-	tippy_obj[node.id()] = tip;
-	var nestedtip = makeNestedTippy(shortText, node.data('full_text'));
-}
 
-cy.on('tap', 'node', function(evt){
-	var node = this;
-	if (tippy_obj[node.id()].state.isShown){
-		// console.log('hide');
-		// tippy_obj[node.id()] = makeTippy(node, 'expect');
-		// var tippy = tippy_obj[node.id()];
-		tippy_obj[node.id()].hide();
-	} else {
-		// console.log('show');
-		// var tippy = tippy_obj[node.id()];
-		tippy_obj[node.id()].show()
+var toJson = function(obj){ return obj.json(); };
+var graphP = fetch('tippy-test-graph.json').then(toJson);
+Promise.all([ graphP]).then(initCy);
+
+function initCy(then){
+	var elems = then[0]
+	var cy = window.cy = cytoscape({
+	container: document.getElementById('cy'),
+
+	style: [
+		{
+			selector: 'node',
+			style: {
+				'content': 'data(id)'
+			}
+		},
+
+		{
+			selector: 'edge',
+			style: {
+				'curve-style': 'bezier',
+				'target-arrow-shape': 'triangle',
+				'content': 'data(id)'
+			}
+		}
+	],
+
+	elements: elems,
+	layout: {
+		name: 'grid'
+	}
+	});
+
+	// create tippy for every node
+	
+	for (var i = 0; i < cy.nodes().size(); i++){
+		var node = cy.nodes()[i];
+		var tip = makeTippy(node, node.data('summary'));
+		tippy_obj[node.id()] = tip;
+		var nestedtip = makeNestedTippy(shortText, node.data('full_text'));
 	}
 
-	
-});
+	cy.on('tap', 'node', function(evt){
+		var node = this;
+		if (tippy_obj[node.id()].state.isShown){
+			// console.log('hide');
+			// tippy_obj[node.id()] = makeTippy(node, 'expect');
+			// var tippy = tippy_obj[node.id()];
+			tippy_obj[node.id()].hide();
+		} else {
+			// console.log('show');
+			// var tippy = tippy_obj[node.id()];
+			tippy_obj[node.id()].show()
+		}
+		// add condition: center if the node is at the margin
+		// cy.center(node)
+	});
+
+}
+
+
+
+
