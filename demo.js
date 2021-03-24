f5205a72e31e8af1ea7daf02ffb13f4b8ec45b78
pikachupichu25 <80876690+pikachupichu25@users.noreply.github.com>
2021-03-24 19:17:18 +0700

    Add files via upload

--- demo.js
+++ demo.js
@@ -2,6 +2,7 @@ var tippy_obj=[];
 var shortText;
 var shownTippy;
 var addButton;
+var layout_config;
 
 // hyperscript-like function
 var h = function(tag, attrs, children){
@@ -85,20 +86,30 @@ var graphP = fetch('tippy-test-graph.json').then(toJson);
 var styleP = fetch('graph-style.json').then(toJson)
 Promise.all([graphP, styleP]).then(initCy);
 
-var layout_config = {
-			name: 'cola', //check https://blog.js.cytoscape.org/2020/05/11/layouts/ cola, fcose
-			animate: true,
-			padding: 50,
-			nodeSpacing: function( node ){ return 90; }
-		}
+
+//check https://blog.js.cytoscape.org/2020/05/11/layouts/ cola, fcose
+
+
+
+
 function initCy(then){
 	var elems = then[0]
 	var cy = window.cy = cytoscape({
 	container: document.getElementById('cy'),
 		style: then[1],
 		elements: elems,
-		layout: layout_config
 	});
+
+	document.getElementById("layoutButton").addEventListener("click", function(){
+	let layoutType = document.getElementById("layout-type")
+	layout_config = {
+			name: layoutType.options[layoutType.selectedIndex].value, 
+			animate: true,
+			nodeSpacing: function( node ){ return 90; }
+		}
+	var layout = cy.layout(layout_config);
+	layout.run()
+	})
 
 
 	cy.on('mouseover', 'node', function(evt){
@@ -143,9 +154,19 @@ function initCy(then){
 
 	cy.on('doubleTap', 'node', function(evt){
 		// generate nodes up to 
-		console.log('doubletap on node')
 		var node = this;
-		addNode(cy, node, 'dummy', 'dummy summary');
+		var branch_size = node.connectedEdges().size();
+		var new_node_num;
+		if (branch_size < 2) {
+			new_node_num = 3-branch_size;
+		} else {
+			new_node_num = 2;
+		}
+		var i;
+		for (i=0; i<new_node_num; i++) {
+			addNode(cy, node, 'dummy', 'dummy summary');
+		}
+
 
 	});
 };
